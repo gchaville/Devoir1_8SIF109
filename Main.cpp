99714649cb76a991e48176463345a0a1e5127b94
culmou1 <daehlinadeauotis@gmail.com>
2015-02-08 11:11:10 -0500

    Ajout de commentaire
    J’ai ajouté plusieurs commentaire

--- Main.cpp
+++ Main.cpp
@@ -111,7 +111,7 @@ void DossierProfesseur::supprimer (std::string nom, std::string prenom )
 
 std::string DossierProfesseur::afficherleprofplusetudiant ( ) const
 {
-	int nb_etudiants, tampon;
+	int nb_etudiants, tampon; /* Tampon comparaison etre le professeur */
 	Professeur *profrecherche;
 	std::string nomrecherche;
 
@@ -119,7 +119,7 @@ std::string DossierProfesseur::afficherleprofplusetudiant ( ) const
 	courant_etu = courant_prof->listetudiant;
 
 	for (nb_etudiants = 0; courant_etu->apres != NULL; nb_etudiants++)
-		{
+		{ /* Compte le nombre de d'etudiant */
 			courant_etu = courant_etu->apres;
 		}
 	courant_prof = courant_prof->suivant;
@@ -131,18 +131,18 @@ std::string DossierProfesseur::afficherleprofplusetudiant ( ) const
 		{
 			courant_etu = courant_etu->apres;
 		}
-		if (tampon > nb_etudiants)
+		if (tampon > nb_etudiants) /* Si tampon est plus grand */
 		{
-			nb_etudiants = tampon;
-			profrecherche = courant_prof;
+			nb_etudiants = tampon; /* On donne le nombre a nb_etudiant */
+			profrecherche = courant_prof; /* On donne le nom du prof au pointeur rechercher */
 		}
 		else if (tampon == nb_etudiants && courant_prof->prenom != profrecherche->prenom)
-		{
+		{ /* Condition pour 2 professeur avec le même nombre d'etudiant */
 			nomrecherche = courant_prof->nom + " " + courant_prof->prenom + " et ";
 		}
 		courant_prof = courant_prof->suivant;
 	}
-	nomrecherche += profrecherche->nom + " " + profrecherche->prenom;
+	nomrecherche += profrecherche->nom + " " + profrecherche->prenom; /* Donne le nom et prenom */
 	courant_prof = tete;
 	return nomrecherche;
 }
@@ -157,8 +157,8 @@ int DossierProfesseur::affichernbreprofpouruncours (std::string coursdonne) cons
 		courant_cours = courant_prof->listecours;
 		while (courant_cours != NULL)
 		{
-			if (coursdonne  == courant_cours->sigle)
-				nbprofpourcours += 1;
+			if (coursdonne  == courant_cours->sigle) /* Si le nom de la variable recus*/
+				nbprofpourcours += 1; /* Incrémentation dans nôtre compteur*/
 			courant_cours = courant_cours->suivant;
 		}
 		courant_prof = courant_prof->suivant;
@@ -180,16 +180,16 @@ std::string DossierProfesseur::affichercoursplusdemande ( ) const
 	courant_cours = courant_prof->listecours;
 	coursrecherche = courant_prof->listecours;
 	nbre = affichernbreprofpouruncours (courant_cours->sigle);
-
+	/*Le nombre est égal au cours le plus demander par les professeurs */
 	while (courant_prof != NULL)
-	{
+	{/* Boucle qui traverse nôtre liste chaine */
 		courant_cours = courant_prof->listecours;
 		while (courant_cours != NULL)
-		{
+		{/* Liste qui traverse les cours */
 			tampon = affichernbreprofpouruncours (courant_cours->sigle);
-			if (nbre < tampon)
+			if (nbre < tampon) /* Vérifie si nbre est inférieur a tampon */
 			{
-				nbre = tampon;
+				nbre = tampon; /* Recois la valeur de nbre */
 				coursrecherche = courant_cours;
 			}
 			courant_cours = courant_cours->suivant;
@@ -207,27 +207,27 @@ std::string DossierProfesseur::affichercoursplusdemande ( ) const
 
 void DossierProfesseur::recopier (std::string FP)
 {
-	std::ofstream ficEcriture(FP,std::ios::out); /* je n'es peut-être pas utilisé le bon mode */
+	std::ofstream ficEcriture(FP,std::ios::out);
     courant_prof = tete;
     while (courant_prof != NULL) // Ouverture en monde écriture
     {
         ficEcriture << courant_prof->nom << std::endl; // mets la valeur de courant_prof->Nom
 		ficEcriture << courant_prof->prenom << std::endl; // Mets la valeur de courant_prof ->prenom
         courant_cours=courant_prof->listecours;
-        while (courant_cours->sigle != "&")
+        while (courant_cours->sigle != "&") /* Doit toucher un '&' pour arrêter*/
         {
             ficEcriture << courant_cours->sigle << std::endl;
             courant_cours=courant_cours->suivant;
         }
-        ficEcriture << "&" << std::endl;
+        ficEcriture << "&" << std::endl; /* Écriture de '&' dans le fichier */
         courant_etu=courant_prof->listetudiant;
-        while (courant_etu->nom != "&")
+        while (courant_etu->nom != "&") /* Doit toucher un '&' pour arrêter*/
         {
             ficEcriture << courant_etu->nom << std::endl;
             courant_etu=courant_etu->apres;
         }
-        ficEcriture << "&";
-		if (courant_prof->suivant != NULL)
+        ficEcriture << "&"; /* Écriture de '&' dans le fichier */
+		if (courant_prof->suivant != NULL) /* Vérification si la chaine continu */
 			ficEcriture << std::endl;
         courant_prof = courant_prof->suivant;
 	}
@@ -238,40 +238,41 @@ void DossierProfesseur::recopier (std::string FP)
 void main ()
 {
 	std::string cours;
-    std::string desnom, desprenom;
-	std::ifstream ft_entree;
+    std::string desNom, desPrenom; /* Détruire le nom et prenom d'un prof */
+	std::ifstream ft_entree; /* Fichier dans le quel contient les instruction */
 	char choix;
 	int coursplusdemande;
-	DossierProfesseur first ("FP.txt");
+	DossierProfesseur first ("FP.txt"); /* Lecture du Constructeur avec le fichier "FP.TXT" */
 
-	afficher ();
-	ft_entree.open ("test.txt");
+	afficher (); /* Permet de voir le fichier lu avec les informations*/
+	ft_entree.open ("test.txt"); /* Ouverture des instructions */
 	if (ft_entree.fail ())
 	{
 		std::cout << "Erreur d'ouverture";
+		ft_entree.close();
 	}
 	else // CREE TETE LISTE
 	{
-		while (!ft_entree.eof())
+		while (!ft_entree.eof()) /* tant que le fichier d'instruction n'a pas termine */
 		{
 			ft_entree >> choix;
 			switch (choix)
 			{
-			case '-':ft_entree >> desnom >> desprenom;
-					 first.supprimer(desnom, desprenom);
+			case '-':ft_entree >> desNom >> desPrenom;
+					 first.supprimer(desNom, desPrenom); /* Appel d'une fonction */
 					 break;
-			case '#':std::cout << std::endl << "Le/s prof/s qui a le plus d'etudiant est/sont " << first.afficherleprofplusetudiant( );
+			case '#':std::cout << std::endl << "Le/s prof/s qui a le plus d'etudiant est/sont " << first.afficherleprofplusetudiant( ); /* Appel d'une fonction */
 					 break;
-			case '*':std::cout << std::endl << "Le cours le plus demande est " << first.affichercoursplusdemande ( );
+			case '*':std::cout << std::endl << "Le cours le plus demande est " << first.affichercoursplusdemande ( ); /* Appel d'une fonction */
 					 break;
 			case '%':ft_entree >> cours;
-					 coursplusdemande = first.affichernbreprofpouruncours (cours);
+					 coursplusdemande = first.affichernbreprofpouruncours (cours); /* Appel d'une fonction */
 					 if (coursplusdemande == 0)
 						 std::cout << std::endl << "Ce cours n'existe pas";
 					 else
 						std::cout << std::endl << "Le nombre de profs pour le cours " << cours << " est de " << coursplusdemande;
 					 break;
-			case '$':first.recopier  ("FP.txt");
+			case '$':first.recopier  ("FP.txt"); /* Appel d'une fonction */
 					 break;
 			default: std::cout << std::endl << "L'operation demandee n'est pas executable !";
 					 break;
